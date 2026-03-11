2abce0608461403ad5b906828383deec32fe4353
Hyunjae Cho <hyunjaecho115@gmail.com>
2026-03-10 20:26:06 -0400

    modified
    modified

--- global_simu1.R
+++ global_simu1.R
@@ -16,7 +16,7 @@ source('funcs/bivar.gc.fit.R')
 ################################################################################
 simulation <- function(iter){
   set.seed(iter)
-  # Generate simulation data
+  # Generate datasets and true coefficient parameters
   dat <- simul.data.gen(n, Tr, Ver, V.all)
   Y <- dat$Y 
   X <- dat$X 
@@ -27,12 +27,11 @@ simulation <- function(iter){
   beta.true <- dat$bivar.beta
   bivar.true <- cbind(alpha.true, beta.true)
   
-  # bootstrap sampling
   lambda <- 10^(seq(-6, 0, by = 1))
   Y.est <- Y[,ind.inside]
   V <- V.all[ind.inside, ]
   
-  # get basis
+  # Construct bivariate spline basis functions over the triangulation
   if(d > 1){
     Ball <- basis(Ver, Tr, d, r, V)
     K <- Ball$K
@@ -40,11 +39,13 @@ simulation <- function(iter){
     B <- Ball$B 
   }
   
+  # Estimate bivariate coefficient functions
   X_tilde <- M * X
   Y_tilde <- M * Y.est
   mfit0 <- bivar.gc.fit(B, Q2, K, lambda, X_tilde, Y_tilde)
   bivar.est <- mfit0$beta
   
+  # Bootstrap
   boot_beta <- array(NA_real_, dim = c(nrow(bivar.est), ncol(bivar.est), nboot))
   for(b in 1:nboot){
     sampling <- sample(nrow(X), replace = TRUE)
@@ -56,19 +57,18 @@ simulation <- function(iter){
     Y_tilde.s <- M.s * Y.est.s
     boot_beta[,,b] <- bivar.gc.fit(B, Q2, K, lambda, X_tilde.s, Y_tilde.s)$beta
   }
-  # obtain bootstrap variance matrix
+  # Obtain bootstrap variance matrix
   variance.mat <- apply(boot_beta, c(1,2), var, na.rm = TRUE)
   se.mat <- apply(boot_beta, c(1,2), sd, na.rm = TRUE)
-  # obtain coverage probability
-  # 95% coverage rate
+  # Obtain coverage probability (95%)
   coverage.prob.lower_95 <- bivar.est + qnorm(0.05/2)*se.mat
   coverage.prob.upper_95 <- bivar.est + qnorm(1-0.05/2)*se.mat
   covered_95 <- (bivar.true[ind.inside,] >= coverage.prob.lower_95) & (bivar.true[ind.inside,] <= coverage.prob.upper_95)
   covered.mean_95 <- colMeans(covered_95)
   
-  # obtain bias
+  # Calculate bias
   bias.res <- apply((bivar.est - bivar.true[ind.inside,]), 2, mean, na.rm = TRUE)
-  # obtain mise
+  # Calculate MISE
   mse.res <- apply((bivar.est - bivar.true[ind.inside,])^2, 2, mean, na.rm = TRUE)
   return(list(bias = bias.res, MISE = mse.res,
               coverage.rate.95=covered.mean_95, 
@@ -76,11 +76,12 @@ simulation <- function(iter){
               variance = colMeans(variance.mat)))
 }
 
+# Initialize model parameters (Sample size, degree, and smoothness)
 nT <- 6
 n <- 100; d <- 2; r <- 1; alpha <- 0.05
 nboot<- 100
 
-# Set boundary 
+# Define 2D spatial grid and extract coordinate vectors
 xm <- seq(-1, 3.5, length = 101)
 yn <- seq(-1, 1, length = 101)
 xy_grid <- pracma::meshgrid(xm, yn)
@@ -88,7 +89,7 @@ uu <- c(xy_grid$X)
 vv <- c(xy_grid$Y)
 V.all <- as.matrix(cbind(uu,vv))
 
-# Load horseshoe and find triangulation 
+# Load horseshoe domain and construct triangulation mesh
 bb <- Triangulation::hs
 VT <- TriMesh(bb, n = nT)
 Tr <- as.matrix(VT$Tr) 
@@ -101,6 +102,7 @@ cr_mat_95  <- do.call(rbind, lapply(res, `[[`, "coverage.rate.95"))
 var_mat  <- do.call(rbind, lapply(res, `[[`, "variance"))
 cr_95_map <- Reduce("+", lapply(res, `[[`, 'covered_95_mat')) / length(res)
 
+# Results
 round(colMeans(bias_mat),4)
 round(colMeans(MISE_mat),4)
 round(colMeans(var_mat),4)
--- global_simu2.R
+++ global_simu2.R
@@ -18,11 +18,13 @@ source('funcs/gc.fit.ho.R')
 
 simulation <- function(iter){
   set.seed(iter)
-  # Generate simulation data
+  # Generate datasets and true coefficient parameters
   dat <- simul.data.gen(n, Tr, Ver, V.all)
   time.bound<- c(min(dat$tij), max(dat$tij))
   probs <- seq(0, 1, length.out = L + 2)
   time.knots <- quantile(dat$tij, probs = probs)[-c(1, L + 2)]
+  
+  # Construct bivariate spline basis functions over the triangulation
   Basis <- basis.tensor(ss = dat$V, tt = dat$tij, V = Ver, Tri = Tr, 
                         d = d, r = r, time.knots = time.knots, time.bound = time.bound, rho = rho)
   X <- dat$X
@@ -38,11 +40,13 @@ simulation <- function(iter){
   P12 <- Basis$D2
   P2 <- kronecker(diag(ncol(X)), as.matrix(crossprod(Q2, K) %*% Q2))
   
-  # find optimal tuning parameters 
+  # Find optimal tuning parameters 
   cv.fit <- gc.fit.cv(nfold=5,X=X, Y=Y, V=V, M=M, tij=tij)
   lambda11.optimal <- cv.fit$lambda11
   lambda12.optimal <- cv.fit$lambda12
   lambda2.optimal <- cv.fit$lambda2
+  
+  # Estimate coefficient functions
   res <- gc.fit(X, Y, V, M, U0, B0, Q2, K, P11, P12, P2, L, rho,
                  lambda11=lambda11.optimal, lambda12=lambda12.optimal, lambda2=lambda2.optimal)
   bivar.true <- cbind(dat$bivar.alpha, dat$bivar.beta)
@@ -50,6 +54,7 @@ simulation <- function(iter){
   trivar.true <- dat$tri.alpha
   trivar.est <- res$trivar.est
   
+  # Bootstrap
   boot_trivar <- list()
   boot_beta <- array(NA_real_, dim = c(nrow(bivar.est), ncol(bivar.est), nboot))
   for(b in 1:nboot){
@@ -63,21 +68,20 @@ simulation <- function(iter){
     boot_beta[,,b] <- b.res$bivar.est
     boot_trivar[[b]] <- b.res$trivar.est
   }
-  # obtain bootstrap variance matrix
+  # Obtain bootstrap variance matrix
   bivar.se.mat <- apply(boot_beta, c(1,2), sd)
   trivar.se.mat <- apply(simplify2array(boot_trivar), c(1,2), sd)
   bivar.var.mat <- apply(boot_beta, c(1,2), var)
   trivar.var.mat <- apply(simplify2array(boot_trivar), c(1,2), var)
   var.res <- round(c(mean(trivar.var.mat), colMeans(bivar.var.mat)), 4)
   
-  # obtain coverage probability
-  # 95% coverage rate
+  # Obtain coverage probability (95%)
   bivar.coverage.lower.95 <- bivar.est + qnorm(0.05/2)*bivar.se.mat
   bivar.coverage.upper.95 <- bivar.est + qnorm(1-0.05/2)*bivar.se.mat
   bivar.covered.95 <- (bivar.true >= bivar.coverage.lower.95) & (bivar.true <= bivar.coverage.upper.95)
   covered.bivar.95 <- colMeans(bivar.covered.95)
   
-  # Bias and MISE
+  # Calculate Bias
   trivar.all <- c()
   trivar.bias <- c()
   for(i in 1:nrow(trivar.est)){
@@ -85,6 +89,7 @@ simulation <- function(iter){
     trivar.bias[i] <- mean((trivar.est[i, ] - trivar.true[i, ]), na.rm = TRUE)
   }
   
+  # Calculate MISE
   MISE.res <- c(round(c(mean(trivar.all), apply((bivar.true - bivar.est)^2, 2, mean, na.rm = TRUE)), 4))
   names(MISE.res)[1] <- 'alpha0'
   bias.res <- c(round(c(mean(trivar.bias), apply((bivar.true - bivar.est), 2, mean, na.rm = TRUE)), 4))
@@ -95,13 +100,14 @@ simulation <- function(iter){
 }
 
 
+# Initialize model parameters (Sample size, degree, and smoothness)
 L <- 3
 rho <- 3
 nT <- 6
 n <- 100; d <- 2; r <- 1
 nboot<- 100
 
-# Set boundary 
+# Define 2D spatial grid and extract coordinate vectors
 xm <- seq(-1, 3.5, length = 101)
 yn <- seq(-1, 1, length = 101)
 xy_grid <- pracma::meshgrid(xm, yn)
@@ -109,7 +115,7 @@ uu <- c(xy_grid$X)
 vv <- c(xy_grid$Y)
 V.all <- as.matrix(cbind(uu,vv))
 
-# Load horseshoe and find triangulation 
+# Load horseshoe domain and construct triangulation mesh
 bb <- Triangulation::hs
 VT <- TriMesh(bb, n = nT)
 Tr <- as.matrix(VT$Tr) 
@@ -117,6 +123,7 @@ Ver <- as.matrix(VT$V)
 
 res <- lapply(1:100, function(iter) simulation(iter))
 
+# Results
 bias_mat <- do.call(rbind, lapply(res, `[[`, "bias"))
 MISE_mat <- do.call(rbind, lapply(res, `[[`, "MISE"))
 var_mat  <- do.call(rbind, lapply(res, `[[`, "variance"))
